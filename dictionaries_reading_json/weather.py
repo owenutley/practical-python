fb56570e8d6abf76b45d007e66411cadf47bc8ac
Owen Utley <owen.utley17@gmail.com>
2021-11-09 14:38:39 -0700

    added a weather finder for Salt Lake, and a dice game

--- dictionaries_reading_json/weather.py
+++ dictionaries_reading_json/weather.py
@@ -1,9 +1,26 @@
 import requests
 
-api_key = "1141afdd1ead5402c22a9aead0dd7ae1"
-city = "Orlando"
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
+def get_weather():
+    api_key = "1141afdd1ead5402c22a9aead0dd7ae1"
+    city = "Salt Lake"
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=imperial"
 
-request = requests.get(url)
-json = request.json()
-print(json)
+    request = requests.get(url)
+    json = request.json()
+    print(json)
+
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {'description': description,
+    'temp_min': temp_min,
+    'temp_max': temp_max}
+
+def main():
+    weather_dict = get_weather()
+    print("Today's forcast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum temperature is", weather_dict.get('temp_max'))
+
+main()
---
+++ guessing_game.py
@@ -0,0 +1,9 @@
+import random
+
+name = str(input("Please enter a name!"))
+
+print(name, "I am thinking of a number between 1 and 100.\n" + "Try to guess My number")
+
+number = 0
+
+while number 
